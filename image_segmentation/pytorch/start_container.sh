3a1356885a9bf6152f41daf51396e42f140c163e
Loic Ho-Von <loic.hovon@gmail.com>
2022-04-16 12:49:05 -0400

    create needed directories if don't exist

--- image_segmentation/pytorch/start_container.sh
+++ image_segmentation/pytorch/start_container.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [[ ! -d results ]]
+    mkdir results
+fi
+if [[ ! -d ckpts ]]
+    mkdir ckpts
+fi
+
 docker run --ipc=host -it --rm --runtime=nvidia -v /data/kits19/data/:/raw_data -v /data/kits19/preprocessed_data/:/data -v $(pwd)/results:/results -v $(pwd)/ckpts:/ckpts unet3d:latest /bin/bash
