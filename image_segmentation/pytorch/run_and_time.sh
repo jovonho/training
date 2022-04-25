eac851570a669b0a8c574d0a8523d5c1bc0cb465
Loic Ho-Von <loic.hovon@gmail.com>
2022-04-25 11:22:28 -0400

    no nvidia-smi available in container

--- image_segmentation/pytorch/run_and_time.sh
+++ image_segmentation/pytorch/run_and_time.sh
@@ -8,7 +8,7 @@ set -e
 SEED=${1:--1}
 # If second argument is given, interpret it as the world size (num GPUs) for experiments
 # It's exported as a environment variable. Default is one
-export WORLD_SIZE=${2:1}
+export WORLD_SIZE=${2:-1}
 
 # Hard coded values for experiments
 MAX_EPOCHS=100
