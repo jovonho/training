7f15eb4111352313a46e8177af036bb26b04253b
Loic Ho-Von <loic.hovon@gmail.com>
2022-04-29 09:06:26 -0400

    change where directories are created

--- image_segmentation/pytorch/start_training.sh
+++ image_segmentation/pytorch/start_training.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-if [[ ! -d results ]]
-then
-    mkdir results
-fi
-
-if [[ ! -d ckpts ]]
-then
-    mkdir ckpts
-fi
-
 num_gpus=${1:-4}
 workload_dir="/mlcommons_training/image_segmentation/pytorch"
+
+
+if [[ ! -d "${workload_dir}/results" ]]
+then
+    mkdir "${workload_dir}/results"
+fi
+
+if [[ ! -d "${workload_dir}/ckpts" ]]
+then
+    mkdir "${workload_dir}/ckpts"
+fi
+
 
 if [ -z $2 ]
 then
@@ -24,7 +26,7 @@ then
 		unet3d:latest /bin/bash run_and_time.sh 1 $num_gpus
 else
 	mem_limit=$2
-
+	# Does not work for now on AWS, since we don't have a swap partition
 	docker run --ipc=host --name=training -it --rm --runtime=nvidia \
 		--memory="${mem_limit}" \
 		--memory-swap="-1" \
