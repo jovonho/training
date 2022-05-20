fe940b4dc3fc409202f3354248112f72432a3844
Loic Ho-Von <loic.hovon@gmail.com>
2022-05-19 20:35:23 -0400

    Added launch scripts

---
+++ image_segmentation/pytorch/start_container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ ! -d results ]]
+then
+    mkdir results
+fi
+
+if [[ ! -d ckpts ]]
+then
+    mkdir ckpts
+fi
+
+docker run --ipc=host --name=training -it --rm --runtime=nvidia -v /data/kits19/data/:/raw_data -v /data/kits19/preprocessed_data/:/data -v $(pwd)/results:/results -v $(pwd)/ckpts:/ckpts unet3d2:latest /bin/bash
---
+++ image_segmentation/pytorch/start_training.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+num_gpus=${1:-4}
+workload_dir="/mlcommons_training/image_segmentation/pytorch"
+
+if [[ ! -d "${workload_dir}/results" ]]
+then
+    mkdir "${workload_dir}/results"
+fi
+
+if [[ ! -d "${workload_dir}/ckpts" ]]
+then
+    mkdir "${workload_dir}/ckpts"
+fi
+
+docker run --ipc=host --name=training -it --rm --runtime=nvidia \
+	-v /data/kits19/data/:/raw_data \
+	-v /data/kits19/preprocessed_data/:/data \
+	-v ${workload_dir}/results:/results \
+	-v ${workload_dir}/ckpts:/ckpts \
+	unet3d2:latest /bin/bash run_and_time.sh 1 $num_gpus
